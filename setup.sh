b21c93f09d38646bb7046b3e187eaa887a89c3e8
Prateek Goyal <prateekg045@gmail.com>
2020-08-16 16:10:06 +0530

    Added heroku deployment files

---
+++ setup.sh
@@ -0,0 +1,32 @@
+# mkdir -p ~/.streamlit/
+
+# echo "\
+# [general]\n\
+# email = \"prateekg045@gmail.com\"\n\
+# " > ~/.streamlit/credentials.toml
+
+# echo "\
+# [server]\n\
+# headless = true\n\
+# enableCORS=false\n\
+# port = $PORT\n\
+# " > ~/.streamlit/config.toml
+
+
+# # mkdir -p ~/.streamlit/
+# # echo "
+# # [server]\n
+# # headless = true\n
+# # enableCORS=false\n
+# # port = $PORT\n
+# # \n
+# # " > ~/.streamlit/config.toml
+
+
+mkdir -p ~/.streamlit
+
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
+" > ~/.streamlit/config.toml
